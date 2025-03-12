b383aa483190b52f10b9c4589cdd6955249e1140
sergio <sergio@sergio-Satellite-L300>
2025-03-12 14:29:16 +0100

    https://github.com/sdaiproyecto1979maker/SDAICore/issues/19
    Desarrollar las conexiones a las bases de datos

--- README.md
+++ README.md
@@ -12,6 +12,7 @@
 * Crear nueva clase de utilidades que transforme instancias de un tipo objeto en otro
 * Añadir un nuevo metodo en la clase de lectura de documentos XML que nos devuelva como un entero el valor de un nodo
 * Crear una clase de utilidades para gestionar las horas
+* Desarrollar las conexiones a las bases de datos
 
 # 0.0.0.0-RELEASE
 * Commit con la configuración inicial
--- src/main/java/sdai/com/sis/conexiones/Conexion.java
+++ src/main/java/sdai/com/sis/conexiones/Conexion.java
@@ -1,6 +1,13 @@
 package sdai.com.sis.conexiones;
 
+import java.util.List;
+import java.util.Map;
+
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
+import sdai.com.sis.accesoadatos.IEntidad;
+import sdai.com.sis.utilidades.Hora;
 
 /**
  * @date 12/03/2025
@@ -10,9 +17,75 @@ import jakarta.persistence.EntityManager;
 public final class Conexion {
 
 	private final EntityManager entityManager;
+	private final Hora horaDCreacion;
 
 	Conexion(BaseDDatos baseDDatos) {
 		this.entityManager = baseDDatos.createEntityManager();
+		this.horaDCreacion = Hora.getHoraDSistema();
+	}
+
+	Boolean isConexionCaducada() {
+		Hora horaDSistema = Hora.getHoraDSistema();
+		Integer minutosTranscurridos = this.horaDCreacion.getMinutosTranscurridos(horaDSistema);
+		return minutosTranscurridos > Integer.valueOf(30);
+	}
+
+	List<IEntidad> ejecutarConsulta(IdQuery idQuery) {
+		String namedQuery = idQuery.getNamedQuery();
+		Map<String, Object> parametrosDQuery = idQuery.getParametrosDQuery();
+		TypedQuery<IEntidad> query = this.entityManager.createNamedQuery(namedQuery, IEntidad.class);
+		for (Map.Entry<String, Object> mapa : parametrosDQuery.entrySet()) {
+			String key = mapa.getKey();
+			Object value = mapa.getValue();
+			query.setParameter(key, value);
+		}
+		List<IEntidad> resultados = query.getResultList();
+		return resultados;
+	}
+
+	void ejecutarPersist(IEntidad entidad) {
+		EntityTransaction entityTransaction = this.entityManager.getTransaction();
+		if (!entityTransaction.isActive())
+			entityTransaction.begin();
+		this.entityManager.persist(entidad);
+	}
+
+	void ejecutarMerge(IEntidad entidad) {
+		EntityTransaction entityTransaction = this.entityManager.getTransaction();
+		if (!entityTransaction.isActive())
+			entityTransaction.begin();
+		this.entityManager.merge(entidad);
+	}
+
+	void ejecutarRemove(IEntidad entidad) {
+		EntityTransaction entityTransaction = this.entityManager.getTransaction();
+		if (!entityTransaction.isActive())
+			entityTransaction.begin();
+		this.entityManager.remove(entidad);
+	}
+
+	void doCommit() {
+		EntityTransaction entityTransaction = this.entityManager.getTransaction();
+		if (entityTransaction.isActive())
+			entityTransaction.commit();
+	}
+
+	void doRollback() {
+		EntityTransaction entityTransaction = this.entityManager.getTransaction();
+		if (entityTransaction.isActive())
+			entityTransaction.rollback();
+	}
+
+	void liberarConexion() {
+		this.entityManager.clear();
+	}
+
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
+
+	public Hora getHoraDCreacion() {
+		return horaDCreacion;
 	}
 
 }
