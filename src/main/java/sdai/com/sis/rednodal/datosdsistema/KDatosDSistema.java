12db51f45429f3ad85b08acaf41c83dcbe9fa80f
CP8WDSENDASMP01\smp <sergio.munoz@sisnet360.com>
2025-03-13 08:32:39 +0100

    Commit parcial

---
+++ src/main/java/sdai/com/sis/rednodal/datosdsistema/KDatosDSistema.java
@@ -0,0 +1,24 @@
+package sdai.com.sis.rednodal.datosdsistema;
+
+/**
+ * @date 13/03/2025
+ * @since VERSIONDCOREENCURSO
+ * @author Sergio_M
+ */
+public abstract class KDatosDSistema {
+
+	public abstract class KDatoDSistema {
+
+		/** Nombre de la tabla */
+		public static final String NOMBRTABLA = "TBDATSISTE";
+
+		public abstract class AtributosDEntidad {
+
+			/** Identificador de dato de sistema */
+			public static final String IDDATSISTE = "IDDATSISTE";
+			/** Código de dato de sistema */
+			public static final String CODIGODATO = "CODIGODATO";
+		}
+	}
+
+}
